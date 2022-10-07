c52e95ec8d8d113d4bdff3de2125fdb54a6d2684
Bruno Leite <leite.bruno5@gmail.com>
2022-10-06 22:59:23 -0300

    Create .NET Weather Forecast project

---
+++ src/WeatherForecast/WeatherForecast.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherForecast.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private static readonly string[] Summaries = new[] {
+            "Freezing",
+            "Bracing",
+            "Chilly",
+            "Cool",
+            "Mild",
+            "Warm",
+            "Balmy",
+            "Hot",
+            "Sweltering",
+            "Scorching"
+        };
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                var weatherForecast = Enumerable.Range(1, 5).Select(index => new Models.WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                }).ToArray();
+
+                return Ok(weatherForecast);
+            }
+            catch (Exception ex)
+            {
+                var errorTitle = "Error getting weather forecast data.";
+                _logger.LogError(ex, errorTitle);
+                var problemDetails = new ProblemDetails
+                {
+                    Status = StatusCodes.Status400BadRequest,
+                    Title = errorTitle,
+                    Detail = ex.Message,
+                    Instance = HttpContext.Request.Path
+                };
+
+                return BadRequest(problemDetails);
+            }
+        }
+    }
+}
